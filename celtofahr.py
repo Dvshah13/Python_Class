aad4a9648571fa9398c4389d32f072c5a707b9c7
Dvshah13 <dvshah13@gmail.com>
2016-12-07 14:51:58 -0600

    Basic Python Celcius to Farenheit converter
    Just a basic temperature converter

---
+++ celtofahr.py
@@ -0,0 +1,5 @@
+celsius = float(raw_input("What is your temperature in celcius? "))
+
+fahrenheit = (celsius * 1.8) + 32
+
+print('%0.1f degree Celsius is equal to %0.1f degree Fahrenheit' %(celsius,fahrenheit))
